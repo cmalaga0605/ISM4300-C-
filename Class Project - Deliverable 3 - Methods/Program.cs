866cd22fd97ea94918008c81edcdd17bdf800bb7
Cristian Malaga <47251490+cmalaga0605@users.noreply.github.com>
2019-09-18 22:10:21 -0400

    Add files via upload

---
+++ Class Project - Deliverable 3 - Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class_Project___Deliverable_3___Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyName();
+        }
+        public static void MyName()
+        {
+            string x = Console.ReadLine();
+            Console.WriteLine("Hello " + x + " I hope your day is wonderful!");
+        }
+
+    }
+}
+
+
